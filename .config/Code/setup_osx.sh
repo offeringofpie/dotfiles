1462524ef3daf1ab49626ef1c0ee6a19c67fde1a
J Lopes <joao.lopes@showpad.com>
2020-05-10 10:43:35 +0100

    setup_osx.sh : rs -rf

--- .config/Code/setup_osx.sh
+++ .config/Code/setup_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm ~/Library/Application\ Support/Code/User/settings.json
 rm ~/Library/Application\ Support/Code/User/keybindings.json
-rm ~/Library/Application\ Support/Code/User/snippets/
+rm -rf ~/Library/Application\ Support/Code/User/snippets/
 ln -s ~/.config/Code/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/.config/Code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/.config/Code/snippets/ ~/Library/Application\ Support/Code/User
