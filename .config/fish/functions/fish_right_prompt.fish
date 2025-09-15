6274d37ab12f19d945ef4d677ef3b31e55061305
J Lopes <offeringofpie@users.noreply.github.com>
2025-09-15 11:56:25 +0100

    Update fish_right_prompt.fish

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,20 +1,18 @@
 function fish_right_prompt -d "Fast right prompt with git status"
     set -l cmd_duration $CMD_DURATION
     
-    # Command duration display
     if test $cmd_duration -gt 2000
         set_color ff6600  # Cyberpunk orange
         printf "%ds " (math "$cmd_duration / 1000")
         set_color normal
     end
     
-    # Fast git status check
     if git rev-parse --git-dir >/dev/null 2>&1
         set -l dirty_count (git status --porcelain=v1 --untracked-files=no --ignore-submodules=dirty 2>/dev/null | wc -l | string trim)
         
         if test "$dirty_count" != "0"
             set_color ffff00  # Cyberpunk yellow
-            printf " ± %s" $dirty_count
+            printf " ≍ %s" $dirty_count
             set_color normal
         else
             # Show clean status with subtle indicator
