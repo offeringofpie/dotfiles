8ac074e939f644b1196adcb3d02205d13ab026f2
J Lopes <offeringofpie@users.noreply.github.com>
2025-09-15 11:22:11 +0100

    Update fish_right_prompt.fish

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,21 +1,39 @@
+set -g async_prompt_functions get_git_status_for_async
 
-function get_git_status -d "Gets the current git status"
-  if git_repo
-    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-
-
-    if [ "$dirty" != "0" ]
-      set_color -b normal
-      set_color yellow
-      echo "$dirty changed file"
-      if [ "$dirty" != "1" ]
-        echo "s"
-      end
-    end
-    set_color normal
-  end
+function __is_git_repo -d "Quick git repository check"
+    git rev-parse --git-dir >/dev/null 2>&1
 end
 
-function fish_right_prompt -d "Prints right prompt"
-  get_git_status
+function get_git_status_for_async -d "Async git status function"
+    if not __is_git_repo
+        return
+    end
+    
+    set -l status_count (git status --porcelain=v1 --untracked-files=no --ignore-submodules=dirty 2>/dev/null | wc -l | string trim)
+    
+    if test "$status_count" != "0"
+        printf "± %s" $status_count
+    end
+end
+
+function get_git_status_for_async_loading_indicator -d "Loading indicator for git status"
+    if __is_git_repo
+        set_color brblack
+        echo -n " …"
+        set_color normal
+    end
+end
+
+function fish_right_prompt -d "Async right prompt"
+    set -l cmd_duration $CMD_DURATION
+    
+    if test $cmd_duration -gt 2000
+        set_color yellow
+        printf "%ds " (math "$cmd_duration / 1000")
+        set_color normal
+    end
+    
+    set_color yellow
+    echo -n (get_git_status_for_async)
+    set_color normal
 end
